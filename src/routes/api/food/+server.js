89473351c21d184d2e3cc8c3d8a82231061c656f
Edona Shabaj <edosha20@htl-shkoder.com>
2026-03-12 15:32:29 +0100

    setup database connection and secure authentication using ENV variables

--- src/routes/api/food/+server.js
+++ src/routes/api/food/+server.js
@@ -1,9 +1,10 @@
 import pool from '$lib/server/database.js';
 
+
 function checkAuth(request) {
     const auth = request.headers.get('authorization');
     if (!auth || !auth.startsWith('Basic ')) return false;
 
     const [user, pass] = atob(auth.split(' ')[1]).split(':');
-    return user === 'admin' && pass === 'albania2024';
+    return user === process.env.API_USER && pass === process.env.API_PASS;
 }
