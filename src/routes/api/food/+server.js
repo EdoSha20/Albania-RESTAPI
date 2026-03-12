6dd5e5c2203ee6eed05fab2b2a06bd3f9dfd8b38
Edona Shabaj <edosha20@htl-shkoder.com>
2026-03-12 15:24:06 +0100

    add database connection and basic authentication

--- src/routes/api/food/+server.js
+++ src/routes/api/food/+server.js
@@ -1,1 +1,9 @@
 import pool from '$lib/server/database.js';
+
+function checkAuth(request) {
+    const auth = request.headers.get('authorization');
+    if (!auth || !auth.startsWith('Basic ')) return false;
+
+    const [user, pass] = atob(auth.split(' ')[1]).split(':');
+    return user === 'admin' && pass === 'albania2024';
+}
